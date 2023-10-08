fcef8fe9e4081cb58f5482a59361f87607171068
ziko144 <zakirkazimzad19@gmail.com>
2023-10-08 17:57:19 +0400

    add

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 function myFunction(){
-    let a = prompt("Duzbucaqlinin sahesi ?")
-    let b = prompt("duzbucaqlinin sahesi ?")
+    let a = prompt("A-nin uzunlugu ?")
+    let b = prompt("B-nin eni ?")
     let cavab = (a*b)
-    let netice = "2" + a + "(a+b)" 
+    let netice = "Uzunlugu " + a + " olan ve eni  " + b + " olan Duzbucagin  sahesi " + cavab + " dir."
     document.getElementById("buton").innerHTML = netice;
 }
 function myFunction2(){
-    let a = prompt("Ucbucagin sahesi ?")
-    let b = prompt("Ucbucagin sahesi ?")
+    let a = prompt("A-nin Uzunlugu ?")
+    let b = prompt("B-nin Eni ?")
     let cavab = (a*b/2)
-    let netice = "S=" + a + "12ah " 
+    let netice = "Uzunlugu " + a + " olan ve eni " + b + " olan Ucbucagin sahesi " + cavab + " dir."
     document.getElementById("buton2").innerHTML = netice
 }
 function myFunction3(){
-    let a = prompt ("Kvadratin sahesi ?")
+    let a = prompt ("Terefinin Qiymeti ?")
     cavab = (a**2)
-    let netice = "S=" + a + "a4 "
+    let netice = "Tereflerinin qiymeti " + a + " olan kvadratin sahesinin qiymeti " + cavab + " dir."
     document.getElementById("buton3").innerHTML = netice 
 }
